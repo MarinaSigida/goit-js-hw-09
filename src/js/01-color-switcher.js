f8c22093019298bc2881825726bd4912825e307d
MarinaSigida <sigida.web@gmail.com>
2023-06-02 15:31:49 +0200

    task1 done

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,38 +1,36 @@
 const refs = {
   mainBody: document.querySelector('body'),
-  startBtn: document.querySelector('[data-start]'),
-  stopBtn: document.querySelector('[data-stop]'),
+  startBtn: document.querySelector('button[data-start]'),
+  stopBtn: document.querySelector('button[data-stop]'),
 };
 
-let intervalId = null;
+// variable to store our intervalID
+let intervalId;
 
 refs.startBtn.addEventListener('click', onStartBtn);
 refs.stopBtn.addEventListener('click', onStopBtn);
 
 function onStartBtn() {
+  // check if an interval has already been set up
+  if (!intervalId) {
+    intervalId = setInterval(changeColor, 1000);
+    refs.startBtn.setAttribute('disabled', true);
+  }
+}
+
+function changeColor() {
   refs.mainBody.style.backgroundColor = getRandomHexColor();
-  intervalId = setInterval(onStartBtn, 1000);
-  if (onStartBtn) {
-    refs.startBtn.setAttribute('disabled', 'true');
-  };
-};
+}
 
 function onStopBtn() {
   clearInterval(intervalId);
-  refs.startBtn.setAttribute('disabled', 'false');
-};
+  refs.startBtn.removeAttribute('disabled');
+  // release our intervalID from the variable
+  intervalId = null;
+}
 
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
     .padStart(6, 0)}`;
 }
-
-
-// function onStartBtn() {
-//     refs.mainBody.style.backgroundColor = getRandomHexColor();
-//     intervalId = setInterval(onStartBtn, 1000);
-//     if (onStartBtn) {
-//       refs.startBtn.setAttribute('disabled', 'true');
-//     }
-//   }
